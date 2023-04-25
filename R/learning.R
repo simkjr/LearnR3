f72e922d0950428b3b3baf2f325195c3bd1ee174
Simon Kjær Simonsen <simkjr@clin.au.dk>
2023-04-25 15:39:20 +0200

    Commited todo file

---
+++ R/learning.R
@@ -0,0 +1,18 @@
+# Basics of R -------------------------------------------------------------
+
+colnames(airquality)
+
+str(airquality)
+
+summary(airquality)
+
+1 + 1
+
+mean(2:6)
+
+
+# Loading packages --------------------------------------------------------
+
+library(tidyverse)
+
+
