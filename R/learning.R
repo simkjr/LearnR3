dae72cd25f268dc4be6968d277f26009af456e81
Simon Kjær Simonsen <simkjr@clin.au.dk>
2023-04-26 13:59:50 +0200

    Summary stastistics + exercises

--- R/learning.R
+++ R/learning.R
@@ -125,5 +125,17 @@ nhanes_modified <- nhanes_small %>%
 
 nhanes_modified
 
+#Creating summary statistics
+nhanes_small %>%
+    summarize(max_bmi = max(bmi, na.rm = TRUE),
+              min_bmi = min(bmi, na.rm = TRUE))
 
+nhanes_small %>%
+    filter(!is.na(diabetes)) %>%
+    group_by(diabetes) %>%
+    summarise(mean_age = mean(age, na.rm = TRUE),
+              mean_bmi = mean(bmi, na.rm = TRUE)) %>%
+    ungroup()
 
+# Saving data
+readr::write_csv(nhanes_small, here::here("data/nhanes_small.csv"))
